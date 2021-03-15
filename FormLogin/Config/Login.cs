46acc101e671664570a03cc24b33fead160d6d07
vitor pontual <vitorpguedes@me.com>
2021-03-15 17:32:33 -0300

    ADD Validation Email

--- FormLogin/Config/Login.cs
+++ FormLogin/Config/Login.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -43,28 +44,41 @@ namespace FormLogin.Config
         public string SingUp(string email, string password, string repassword)
         {
             have = false;
-            if (password.Equals(repassword))
-            {
-                cmd.CommandText = "INSERT INTO _user values (@email, @password)";
-                cmd.Parameters.AddWithValue("@email", email);
-                cmd.Parameters.AddWithValue("@password", password);
 
-                try
-                {
-                    cmd.Connection = con.connect();
-                    cmd.ExecuteNonQuery();
-                    con.disconnect();
-                    this.message = "Succesful, SignUp complete";
-                    have = true;
-                }
-                catch (SqlException)
-                {
-                    this.message = "Database Internal Error!!!";
-                }
+            SqlDataAdapter da = new SqlDataAdapter("SELECT email FROM _user WHERE email='"+email+"'", con.connect());
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if(dt.Rows.Count >= 1)
+            {
+                this.message = "Error Email Exists!";
             }
             else
             {
-                this.message = "Password Different!!!";
+
+                if (password.Equals(repassword) && password != "")
+                {
+                    cmd.CommandText = "INSERT INTO _user values (@email, @password)";
+                    cmd.Parameters.AddWithValue("@email", email);
+                    cmd.Parameters.AddWithValue("@password", password);
+
+                    try
+                    {
+                        cmd.Connection = con.connect();
+                        cmd.ExecuteNonQuery();
+                        con.disconnect();
+                        this.message = "Succesful, SignUp complete";
+                        have = true;
+                    }
+                    catch (SqlException)
+                    {
+                        this.message = "Database Internal Error!!!";
+                    }
+                }
+                else
+                {
+                    this.message = "Password Different!!!";
+                }
+          
             }
             return message;
 
